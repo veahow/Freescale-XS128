3ef7d21370d9a67b7fe9be3f4800401df946f1b3
veahow <veahow@gmail.com>
2018-04-09 21:03:25 +0800

    motor.h completed

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include <hidef.h>
 #include <MC9S12XS128.h>
+#include "init.h"
+#include "motor.h"
 
 void main(void)
 {
-    PLL_init()
+    PWM_Init();
+    IO_Init();
+
+    for(;;)
+    {
+
+    }
 }
